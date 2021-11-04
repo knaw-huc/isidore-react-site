c7c81d0e6128ccc1079f4edc72d55d40dada5594
Rob Zeeman <rob.zeeman@di.huc.knaw.nl>
2021-11-04 21:55:05 +0100

    Update config.tsx

--- src/misc/config.tsx
+++ src/misc/config.tsx
@@ -1,6 +1,6 @@
 // Development
- export const SERVICE_SERVER = "http://localhost/isidore_service/";
- export const HOME = "http://localhost:3000/";
+export const SERVICE_SERVER = "http://localhost/isidore_service/";
+export const HOME = "http://localhost:3000/";
 // export const EDITION = "https://localhost/isidore_editie?ms="
 // Test server
 //export const SERVICE_SERVER = "https://isidore.sd.di.huc.knaw.nl/isidore_service/";
@@ -8,8 +8,8 @@
 //export const EDITION = "https://isidore.sd.di.huc.knaw.nl/edition?ms="
 
 // Production
-// export const SERVICE_SERVER = "https://db.innovatingknowledge.nl/isidore_service/";
-// export const HOME = "https://db.innovatingknowledge.nl/";
+//export const SERVICE_SERVER = "https://db.innovatingknowledge.nl/isidore_service/";
+//export const HOME = "https://db.innovatingknowledge.nl/";
 export const EDITION = "https://db.innovatingknowledge.nl/edition?ms="
 // WordPress website
 export const PROJECT_SITE = "https://innovatingknowledge.nl/";
